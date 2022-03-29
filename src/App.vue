43347e189b0cb7b6243b86ad266b561332925bb4
Anu-500apps <anu.taddi@500apps.com>
2022-03-29 18:57:16 +0530

    commited29

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Vue Webpage</h1>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Table-v />
+    <Vfor />
+    <Local :msg="msg" :name="name" :number="number"></Local><br><br>
+    <Child msg="This is static props" name="Anu" number="7330797147"></Child><br />
+    <Bcard />
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import TableV from './components/question1/TableV.vue'
+//import Vfor from './components/questuin2/Vfor.vue'
+//import Local from './components/question3/Local.vue'
+//import Child from './components/question4/Child.vue'
+import Bcard from './components/question6/Bcard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    //HelloWorld,
+    TableV,
+    //Vfor,
+    //Child,
+    //Local,
+    Bcard
+},
+  } 
 </script>
 
 <style>
